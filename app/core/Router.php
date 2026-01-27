2a9ae05390c40840886e04cccdd7ae2c062d314c
virgiliolawrencez-design <virgiliolawrencez@gmail.com>
2026-01-27 14:28:58 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -7,7 +7,24 @@ class Router
 
     public function run()
     {
-     echo 'Router is running';
+     $method = $_SERVER['REQUEST_METHOD'];
+     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+     if ($method == 'GET' && $uri == '/students') {
+         echo "<h1>Daftar Siswa</h1>";
+         echo "<p>Menampilkan daftar siswa</p>";
+         return;
+     }
+
+          if ($method == 'GET' && $uri == '/students/create') {
+         echo "<h1>Tambah siswa</h1>";
+         echo "<p>Menampilkan form tambah siswa</p>";
+         return;
+     }
+
+     http_response_code(404);
+     echo "<h1>404 - Page Not Found</h1>";
+     
     }
 
 }
