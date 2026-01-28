db42dd85622460449719b1183f100b5b2365027e
virgiliolawrencez-design <virgiliolawrencez@gmail.com>
2026-01-28 09:48:58 +0700

    feat:implement simple router

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class StudentController
+{
+    public function index()
+    {
+        echo "<h1>Daftar Siswa</h1>";
+        echo "<p>Menampilkan daftar siswa</p>";
+    }
+    public function create()
+    {
+        echo "<h1>Tambah siswa</h1>";
+        echo "<p>Menampilkan form tambah siswa</p>";;
+    }
+}
--- app/core/Router.php
+++ app/core/Router.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Core;
 
-
+use App\Controllers\StudentController;
 class Router
 {
 
@@ -11,14 +11,16 @@ class Router
      $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
      if ($method == 'GET' && $uri == '/students') {
-         echo "<h1>Daftar Siswa</h1>";
-         echo "<p>Menampilkan daftar siswa</p>";
+         require_once './app/controllers/StudentController.php';
+        $controller = new StudentController();
+        $controller->index();
          return;
      }
 
           if ($method == 'GET' && $uri == '/students/create') {
-         echo "<h1>Tambah siswa</h1>";
-         echo "<p>Menampilkan form tambah siswa</p>";
+                  require_once './app/controllers/StudentController.php';
+        $controller = new StudentController();
+        $controller->create();
          return;
      }
 
